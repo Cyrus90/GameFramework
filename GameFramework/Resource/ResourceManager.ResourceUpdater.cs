ce76faa42d1d0f8d31a27075c55938da6232b5b2
cyrus <836871240@qq.com>
2022-10-12 19:51:45 +0800

    增加CDN解析

--- GameFramework/Resource/ResourceManager.ResourceUpdater.cs
+++ GameFramework/Resource/ResourceManager.ResourceUpdater.cs
@@ -10,6 +10,7 @@ using GameFramework.FileSystem;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace GameFramework.Resource
 {
@@ -672,6 +673,7 @@ namespace GameFramework.Resource
 
                 updateInfo.Downloading = true;
                 string resourceFullNameWithCrc32 = updateInfo.ResourceName.Variant != null ? Utility.Text.Format("{0}.{1}.{2:x8}.{3}", updateInfo.ResourceName.Name, updateInfo.ResourceName.Variant, updateInfo.HashCode, DefaultExtension) : Utility.Text.Format("{0}.{1:x8}.{2}", updateInfo.ResourceName.Name, updateInfo.HashCode, DefaultExtension);
+                resourceFullNameWithCrc32 = GetCDNSafeFull(resourceFullNameWithCrc32);
                 m_DownloadManager.AddDownload(updateInfo.ResourcePath, Utility.Path.GetRemotePath(Path.Combine(m_ResourceManager.m_UpdatePrefixUri, resourceFullNameWithCrc32)), updateInfo);
                 return true;
             }
@@ -1010,6 +1012,47 @@ namespace GameFramework.Resource
                     m_UpdateWaitingInfo.Remove(updateInfo);
                     m_UpdateWaitingInfoWhilePlaying.Remove(updateInfo);
                 }
+            }
+
+            private string GetCDNSafeFull(string url)
+            {
+                long timestamp = ConvertDateTimeToInt(DateTime.Now.AddDays(1));
+                string sid = "10008_5107221990";
+                string key = "cmam2021";
+                string encrpty = EncryptMD5_32(string.Format("{0}{1}{2}{3}", url, timestamp, sid, key));
+                return string.Format("{0}?timestamp={1}&sid={2}&encrypt={3}", url, timestamp, sid, encrpty);
+            }
+
+            /// <summary>
+            /// 将DateTime时间格式转换为Unix时间戳格式
+            /// </summary>
+            /// <param name="time">时间</param>
+            /// <returns>long</returns>
+            private long ConvertDateTimeToInt(DateTime time)
+            {
+                System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+                long t = (time.Ticks - startTime.Ticks) / 10000;  //除10000调整为13位  
+                return t;
+            }
+
+            /// <summary>
+            /// MD5　32位加密
+            /// </summary>
+            /// <param name="_encryptContent">需要加密的内容</param>
+            /// <returns></returns>
+            private string EncryptMD5_32(string _encryptContent)
+            {
+                string content_Normal = _encryptContent;
+                string content_Encrypt = "";
+                MD5 md5 = MD5.Create();
+
+                byte[] s = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(content_Normal));
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    content_Encrypt = content_Encrypt + s[i].ToString("X2");
+                }
+                return content_Encrypt;
             }
         }
     }
